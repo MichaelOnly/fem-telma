1cded7edf68bcda6b62373216c00dd3bd50425f5
Michael <wwwsobaci@mail.ru>
2023-03-14 00:29:15 +0700

    Add input areas class

---
+++ fem-telma/Areas.cs
@@ -0,0 +1,19 @@
+﻿namespace fem_telma;
+
+public class Areas
+{
+    public float[] Xmins { get; set; }
+    public float[] Xmaxs { get; set; }
+    public float[] Ymins { get; set; }
+    public float[] Ymaxs { get; set; }
+    public double[] CurrentDensitys { get; set; }
+    public double[] MagneticPermeabilitys { get; set; }
+    public float[] Xs { get; set; }
+    public float[] XSteps { get; set; }
+    public float[] XRelaxRatios { get; set; }
+    public bool[] IsReversedXRelaxRatio { get; set; }
+    public float[] Ys { get; set; }
+    public float[] YSteps { get; set; }
+    public float[] YRelaxRatios { get; set; }
+    public bool[] IsReversedYRelaxRatio { get; set; }
+}
