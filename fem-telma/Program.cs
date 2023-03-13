725057870b52d0861ee90cc4adbff9efc8300ce5
Michael <wwwsobaci@mail.ru>
2023-03-14 00:26:19 +0700

    Init repo

---
+++ fem-telma/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Text.Json;
+using fem_telma;
+using fem_telma.Grid;
+
+var area = JsonSerializer.Deserialize<Areas>(File.ReadAllText("areas.json"));
+var grid = new Grid(area);
