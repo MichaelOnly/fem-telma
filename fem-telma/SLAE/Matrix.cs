35b7e2e3d7b9540fd989d057e34276f218c38df6
Michael <wwwsobaci@mail.ru>
2023-03-22 23:09:30 +0700

    Add copy matrix function and factorization matrix

--- fem-telma/SLAE/Matrix.cs
+++ fem-telma/SLAE/Matrix.cs
@@ -5,7 +5,7 @@ public class Matrix
     public int[] Ia;
     public int[] Ja;
     public double[] LowTriangleElements;
-    public double[] UpTriangleElements;
+    public double[] UpperTriangleElements;
     public double[] DiagonalElements;
     public int Size;
 
@@ -31,6 +31,88 @@ public class Matrix
             }
         }
         LowTriangleElements = new double[Ia.Last()];
-        UpTriangleElements = new double[Ia.Last()];
+        UpperTriangleElements = new double[Ia.Last()];
+    }
+
+    public Matrix()
+    {
+        Size = 1;
+        DiagonalElements = new double[Size];
+        Ia = new int[Size + 1];
+        Ja = new int[Size];
+        LowTriangleElements = new double[Size];
+        UpperTriangleElements = new double[Size];
+    }
+
+    public double[] MultMatrixOnVector(double[] vec)
+    {
+        var result = new double[vec.Length];
+        for (var i = 0; i < vec.Length; i++)
+        {
+            result[i] = DiagonalElements[i] * vec[i];
+            for (var j = Ia[i]; j < Ia[i+1]; j++)
+            {
+                result[i] += LowTriangleElements[j] * vec[Ja[j]];
+                result[Ja[j]] += UpperTriangleElements[j] * vec[i];
+            }
+        }
+
+        return result;
+    }
+
+    private Matrix CopyMatrix()
+    {
+        var mxCopy = new Matrix();
+        Ia.AsSpan().CopyTo(mxCopy.Ia);
+        mxCopy.Size = Size;
+        Ja.AsSpan().CopyTo(mxCopy.Ja);
+        DiagonalElements.AsSpan().CopyTo(mxCopy.DiagonalElements);
+        LowTriangleElements.AsSpan().CopyTo(mxCopy.LowTriangleElements);
+        UpperTriangleElements.AsSpan().CopyTo(mxCopy.UpperTriangleElements);
+        return mxCopy;
+    }
+    public Matrix LUsqFactorization()
+    {
+        var factorizedMx = CopyMatrix();
+        for (var i = 0; i < factorizedMx.Size; i++)
+        {
+            var sumdi = 0.0;
+            var i0 = factorizedMx.Ia[i];
+            var i1 = factorizedMx.Ia[i + 1];
+
+            for (var k = i0; k < i1; k++)
+            {
+                var j = factorizedMx.Ja[k];
+                var j0 = factorizedMx.Ia[j];
+                var j1 = factorizedMx.Ia[j + 1];
+
+                var ik = i0;
+                var kj = j0;
+
+                var suml = 0.0;
+                var sumu = 0.0;
+                while (ik < k)
+                {
+                    if (factorizedMx.Ja[ik] == factorizedMx.Ja[kj])
+                    {
+                        suml += factorizedMx.LowTriangleElements[ik] * factorizedMx.UpperTriangleElements[kj];
+                        sumu += factorizedMx.UpperTriangleElements[ik] * factorizedMx.LowTriangleElements[kj];
+                        ik++;
+                        kj++;
+                    }
+                    else if (factorizedMx.Ja[ik] > factorizedMx.Ja[kj]) kj++;
+                    else ik++;
+                }
+
+                factorizedMx.LowTriangleElements[k] =
+                    (factorizedMx.LowTriangleElements[k] - suml) / factorizedMx.DiagonalElements[j];
+                factorizedMx.UpperTriangleElements[k] =
+                    (factorizedMx.UpperTriangleElements[k] - sumu) / factorizedMx.DiagonalElements[j];
+                sumdi += factorizedMx.LowTriangleElements[k] * factorizedMx.UpperTriangleElements[k];
+            }
+            factorizedMx.DiagonalElements[i] = Double.Sqrt(factorizedMx.DiagonalElements[i] - sumdi); 
+        }
+
+        return factorizedMx;
     }
 }
