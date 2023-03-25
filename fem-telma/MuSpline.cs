5da77e72522c0acc9206e01aa1e2cf935ba2fe0f
Michael <wwwsobaci@mail.ru>
2023-03-26 00:01:09 +0700

    Add MuSpline.cs for nonlinear task

---
+++ fem-telma/MuSpline.cs
@@ -0,0 +1,41 @@
+﻿namespace fem_telma;
+
+public class MuSpline
+{
+    private double[] _muTable;
+    private double[] _BTable;
+
+    public MuSpline()
+    {
+        using var reader = new StreamReader("mu.002");
+        var countStrings = Int32.Parse(reader.ReadLine());
+        _muTable = new double[countStrings];
+        _BTable = new double[countStrings];
+        for (var i = 0; i < countStrings; i++)
+        {
+            var tableString = reader.ReadLine().Split(' ');
+            _muTable[i] = Double.Parse(tableString[1]);
+            _BTable[i] = Double.Parse(tableString[2]);
+        }
+    }
+
+    public double GetMu(double b)
+    {
+        if (b >= _BTable[^1])
+        {
+            return (_BTable[^1] * (1.0 / _muTable[^1] - 1.0) / b + 1.0) / (4 * Math.PI * 1e-7);
+        }
+
+        var numberSegment = 0;
+        while (b >= _BTable[numberSegment] && b <= _BTable[numberSegment + 1])
+        {
+            numberSegment++;
+        }
+
+        var mu0 = _muTable[numberSegment];
+        var mu1 = _muTable[numberSegment + 1];
+        var b0 = _BTable[numberSegment];
+        var b1 = _BTable[numberSegment + 1];
+        return 1.0 / ((mu0 * ((b1 - b) / (b1 - b0)) + mu1 * ((b - b0) / (b1 - b0))) * 4 * Math.PI * 1e-7);
+    }
+}
--- fem-telma/Program.cs
+++ fem-telma/Program.cs
@@ -7,6 +7,7 @@ using fem_telma.SLAE;
 var area = JsonSerializer.Deserialize<Areas>(File.ReadAllText("areas.json"));
 var points = JsonSerializer.Deserialize<Points>(File.ReadAllText("points.json"));
 var grid = new Grid(area);
+var muSpline = new MuSpline();
 var SLAE = new SLAE(grid);
 SLAE.SolveWithLOSPrecond(1e-15,1000);
 var resFunc = new ResultFunction(SLAE.Result,grid);
